b49f683e81910ccf719497c406afb73a9624cd34
mengyingzhou <myzhou19@fudan.edu.cn>
2025-08-15 18:27:05 +0800

    覆盖vant4默认配色,使用自己定义的全局配色

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
-import 'vant/lib/index.css';
+import 'vant/lib/index.css'
+import './styles/vant-theme.css'
 import router from "@/router"
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import RouterViewKeepAlive from "router-view-keep-alive";
 import kt from 'kitty-ui'
 import Vant from 'vant'
-import 'vant/lib/index.css'
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
